b4764e24ac22e0ff217eadc39dbdf76fc3d55124
M-BEDH <mel.b.ms@pm.me>
2025-09-09 16:06:40 +0200

    first commit - appVoiture

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import { Component } from 'react';
+import Mycars from './components/MyCars';
+import './App.css';
+
+class App extends Component {
+  state = {}
+
+  render() {
+    return (
+      <div>
+        <Mycars />
+
+      </div>
+    )
+  }
+
+}
+export default App;
---
+++ src/components/MyCars.jsx
@@ -0,0 +1,18 @@
+import { Component } from 'react';
+
+class Mycars extends Component {
+  state = {}
+
+  render() {
+    return (
+      <div>
+
+      <h1>Page</h1>
+      <p>test</p>
+
+      </div>
+    )
+  }
+
+}
+export default Mycars;
